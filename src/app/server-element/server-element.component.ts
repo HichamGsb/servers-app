c7e484b27050f0b32e0903b9d4d8fbe4140a1fb6
Hicham Guessab <hicham.guessab@cbainfo.fr>
2023-09-27 14:06:00 +0200

    Accéder au ng-content avec @ContentChild

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,22 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterContentInit, Component, ContentChild, ElementRef, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, AfterContentInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
-
+  @ContentChild('contentParagraph') paragraph: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+    // console.log('Text Content of paragraph ' + this.paragraph.nativeElement.textContent);
+    // -> Undefined à l'initialisation.
+  }
+
+  ngAfterContentInit(): void {
+    console.log('Text Content of paragraph ' + this.paragraph.nativeElement.textContent);
   }
 
 }
